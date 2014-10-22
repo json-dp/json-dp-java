6c3a363730b89398a508b6f1b2520b400ef598c2
Paolo Ciccarese <paolo.ciccarese@gmail.com>
2014-10-21 23:44:24 -0400

    Refactoring of redundant code.

--- src/main/java/info/paolociccarese/project/jsondp/java/core/JsonDpObject.java
+++ src/main/java/info/paolociccarese/project/jsondp/java/core/JsonDpObject.java
@@ -149,24 +149,20 @@ public class JsonDpObject implements JsonDpStream {
 	 * @return The values given the key and provenance data.
 	 */
 	public Object get(Object key, Object provenanceKey, Object... provenanceValues) {
-		JsonDpArray object = new JsonDpArray();
-		for(JsonObjectCore jsonObject: jsonObjects) {
-			if(jsonObject.containsKey(key)) {
-				for(Object provenanceValue: provenanceValues) {
-					if(jsonObject.containsProvenance(provenanceKey, provenanceValue)) {
-						JSONObject prov = new JSONObject();
-						prov.put(provenanceKey, provenanceValue);
-						object.add(jsonObject.getValue(key), prov);
-					}
-				}
-			}
-		}		
+		JsonDpArray object = getValuesWithProvenance(key, provenanceKey, provenanceValues);		
 		JSONObject o = new JSONObject();
 		o.put(key, object.getValues());
 		return o;
 	}
 	
 	public Object getWithProvenance(Object key, Object provenanceKey, Object... provenanceValues) {
+		JsonDpArray object = getValuesWithProvenance(key, provenanceKey, provenanceValues);	
+		JSONObject o = new JSONObject();
+		o.put(key, object.getWithProvenance());
+		return o;
+	}
+	
+	private JsonDpArray getValuesWithProvenance(Object key, Object provenanceKey, Object... provenanceValues) {
 		JsonDpArray object = new JsonDpArray();
 		for(JsonObjectCore jsonObject: jsonObjects) {
 			if(jsonObject.containsKey(key)) {
@@ -178,10 +174,8 @@ public class JsonDpObject implements JsonDpStream {
 					}
 				}
 			}
-		}		
-		JSONObject o = new JSONObject();
-		o.put(key, object.getWithProvenance());
-		return o;
+		}	
+		return object;
 	}
 	
 	public boolean containsKey(Object key) {
